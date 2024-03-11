4e825acf7587b64bab73d5f571cc161c49de9f32
Ayesha Perera <aypdev1995@gmail.com>
2024-03-11 19:20:02 +0000

    create workout form and connect with backend

--- frontend/src/components/WorkoutDetails.js
+++ frontend/src/components/WorkoutDetails.js
@@ -1,11 +1,11 @@
 const WorkoutDetails = ({workout}) => {
-    
+    const { title, load, reps, createdAt } = workout
     return(
        <div className="workout-details">
-        <h4>{workout.title}</h4>
-        <p><strong>Load (kg): </strong>{workout.load}</p>
-        <p><strong>Reps: </strong>{workout.reps}</p>
-        <p>{workout.createdAt}</p>
+        <h4>{title}</h4>
+        <p><strong>Load (kg): </strong>{load}</p>
+        <p><strong>Reps: </strong>{reps}</p>
+        <p>{createdAt}</p>
        </div>
 
     )
