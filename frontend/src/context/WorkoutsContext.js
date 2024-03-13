b36f0be80399cb31af0f073241e84b46307c09de
Ayesha Perera <aypdev1995@gmail.com>
2024-03-13 00:49:12 +0000

    fixed errors

--- frontend/src/context/WorkoutsContext.js
+++ frontend/src/context/WorkoutsContext.js
@@ -1,34 +1,30 @@
-import { createContext, useReducer } from "react";
+import { createContext, useReducer } from 'react'
 
 export const WorkoutsContext = createContext()
 
-export const workoutReducer = (state, action) => {
-    switch(action.type){
-        case 'SET_WORKOUTS':
-            return {
-                workouts: action.payload
-            }
-        case 'CREATE_WORKOUT':
-            return {
-                workouts: [action.payload, ...state.workouts]
-            }
-        default:
-            return state;
-    }
-}
- 
-const WorkoutsContextProvider = ( {children} ) => {
-    const [state, dispatch] = useReducer(workoutReducer, {
-        workouts: null
-    })
-
-    // dispatch({type: 'SET_WORKOUTS', payload: [{},{}]})
-
-    return (
-        <WorkoutsContext.Provider value={{...state, dispatch}}>
-            { children }
-        </WorkoutsContext.Provider>
-    )
+export const workoutsReducer = (state, action) => {
+  switch (action.type) {
+    case 'SET_WORKOUTS':
+      return { 
+        workouts: action.payload 
+      }
+    case 'CREATE_WORKOUT':
+      return { 
+        workouts: [action.payload, ...state.workouts] 
+      }
+    default:
+      return state
+  }
 }
 
-export default WorkoutsContextProvider
+export const WorkoutsContextProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(workoutsReducer, { 
+    workouts: null
+  })
+  
+  return (
+    <WorkoutsContext.Provider value={{ ...state, dispatch }}>
+      { children }
+    </WorkoutsContext.Provider>
+  )
+}
